8b7c46c1c8ef952aacd01dfd36a93ffb6640b7fd
catalamarti <marticatalasabate@gmail.com>
2025-10-05 18:45:06 +0100

    correct urls

---
+++ check_urls.R
@@ -0,0 +1,18 @@
+
+list.files(recursive = TRUE, pattern = "\\.(qmd|md|Rmd)$", full.names = TRUE) |>
+  rlang::set_names() |>
+  purrr::map(\(x) {
+    cont <- readLines(con = x)
+    urls1 <- stringr::str_extract_all(string = cont, pattern = "(?<=<).*?(?=>)") |>
+      unlist() |>
+      unique()
+    urls2 <- stringr::str_extract_all(string = cont, pattern = "(?<=]\\().*?(?=\\))") |>
+      unlist() |>
+      unique()
+    urls <- c(urls1, urls2) |>
+      unique()
+    RCurl::url.exists(urls) |>
+      purrr::keep(\(x) !x) |> 
+      names()
+  })
+
