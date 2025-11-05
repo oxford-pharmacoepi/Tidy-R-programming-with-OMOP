feb07bbc16e677cae780ef3d7f56bdbbe425918f
Ed Burn <9583964+edward-burn@users.noreply.github.com>
2025-11-05 06:49:15 +0000

    style code

--- check_urls.R
+++ check_urls.R
@@ -1,15 +1,20 @@
-
-list.files(recursive = TRUE, pattern = "\\.(qmd|md|Rmd)$",
-           full.names = TRUE) |>
+list.files(
+  recursive = TRUE, pattern = "\\.(qmd|md|Rmd)$",
+  full.names = TRUE
+) |>
   rlang::set_names() |>
   purrr::map(\(x) {
     cont <- readLines(con = x)
-    urls1 <- stringr::str_extract_all(string = cont,
-                                      pattern = "(?<=<).*?(?=>)") |>
+    urls1 <- stringr::str_extract_all(
+      string = cont,
+      pattern = "(?<=<).*?(?=>)"
+    ) |>
       unlist() |>
       unique()
-    urls2 <- stringr::str_extract_all(string = cont,
-                                      pattern = "(?<=]\\().*?(?=\\))") |>
+    urls2 <- stringr::str_extract_all(
+      string = cont,
+      pattern = "(?<=]\\().*?(?=\\))"
+    ) |>
       unlist() |>
       unique()
     urls <- c(urls1, urls2) |>
